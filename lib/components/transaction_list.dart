ae01420455482c145d380e83af92dba5c81019f2
João Vitor <joaovitor.cf97@gmail.com>
2020-12-16 13:49:24 -0300

    Estilo com Theme

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -30,7 +30,7 @@ class TransactionList extends StatelessWidget {
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 20,
-                      color: Colors.purple,
+                      color: Theme.of(context).primaryColor,
                     ),
                   ),
                 ),
